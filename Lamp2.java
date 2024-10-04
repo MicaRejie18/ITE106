2e4478cfa407a89827aecedf673fb4eb363a7ba3
MicaRejie18 <micarejiedames28@gmail.com>
2024-10-04 14:58:30 +0800

    Create Lamp2.java

---
+++ Lamp2.java
@@ -0,0 +1,13 @@
+class Lamp2
+{
+    boolean isOn;
+
+    void turnOn(){
+        isOn = true;
+        System.out.println("Light On? " + isOn);
+    }
+     void turnOff(){
+        isOn = false;
+            System.out.println("Light Off?" + isOn);
+    }
+}
